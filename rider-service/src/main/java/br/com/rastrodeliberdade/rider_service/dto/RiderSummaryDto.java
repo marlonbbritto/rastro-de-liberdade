8001ffb357d6d0c3a974a30df13212ea4a1172f5
Marlon_B_Britto <115008550+marlonbbritto@users.noreply.github.com>
2025-10-05 21:45:37 -0300

    Merge pull request #2
    * feat(Rider): implements RiderInsertDto
    
    * feat(Rider): implements RiderSummaryDto
    
    * feat(Rider): implements RiderMapper

---
+++ rider-service/src/main/java/br/com/rastrodeliberdade/rider_service/dto/RiderSummaryDto.java
@@ -0,0 +1,10 @@
+package br.com.rastrodeliberdade.rider_service.dto;
+
+public record RiderSummaryDto(
+        String bikerNickname,
+        String email,
+        String city,
+        String state
+
+) {
+}
